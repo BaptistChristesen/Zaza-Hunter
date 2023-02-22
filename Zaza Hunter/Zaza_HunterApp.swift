8ff16799d979d6f3a9c8c2c45315ac04392c7c02
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-02-22 15:30:00 -0600

    Initial Commit

---
+++ Zaza Hunter/Zaza_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zaza_HunterApp.swift
+//  Zaza Hunter
+//
+//  Created by caden christesen on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Zaza_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

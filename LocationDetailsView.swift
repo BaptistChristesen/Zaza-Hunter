7bcbbe2af28be2f7c2b4ccc80c530ecefc96ed63
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-02-22 15:44:42 -0600

    added web links and directions and other stuff

--- LocationDetailsView.swift
+++ LocationDetailsView.swift
@@ -7,17 +7,37 @@
 
 import SwiftUI
 import MapKit
-
-struct LocationDetailsView: View {
-    var selectedMapItem: MKMapItem
-    var body: some View {
-        VStack {
-            Text(selectedMapItem.name!)
-                .font(.title)
-        }
-    }
-}
-
+ struct LocationDetailsView: View {
+     var selectedMapItem: MKMapItem
+     var body: some View {
+         let address = selectedMapItem.placemark.subThoroughfare! + " " +    // street address number
+         selectedMapItem.placemark.thoroughfare! + "\n" +                    // street name
+         selectedMapItem.placemark.locality! + ", " +                        // city
+         selectedMapItem.placemark.administrativeArea! + " " +               // state
+         selectedMapItem.placemark.postalCode!                               // zip code
+         VStack {
+             Text(selectedMapItem.name!)
+                 .font(.title)
+             Text(address)
+             Text(selectedMapItem.phoneNumber!)
+                 .padding()
+             Link(destination: selectedMapItem.url!) {
+                 Text("Website")
+                     .padding()
+             }
+             Button {
+                 let latitude = selectedMapItem.placemark.coordinate.latitude
+                 let longitude = selectedMapItem.placemark.coordinate.longitude
+                 let url = URL(string: "maps://?daddr=\(latitude),\(longitude)")
+                 if UIApplication.shared.canOpenURL(url!) {
+                     UIApplication.shared.open(url!, options: [:])
+                 }
+             } label: {
+                 Text("Directions")
+             }
+         }
+     }
+ }
 struct LocationDetailsView_Previews: PreviewProvider {
     static var previews: some View {
         LocationDetailsView(selectedMapItem: MKMapItem())

6031388708770d943276556bef1596c92ce661d5
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-02-22 15:33:40 -0600

    map is set up

--- Zaza Hunter/ContentView.swift
+++ Zaza Hunter/ContentView.swift
@@ -6,21 +6,17 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.0558, longitude: -87.6743), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+       ContentView()
+   }
 }
